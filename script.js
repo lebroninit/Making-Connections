a692fe9c133be9f8f9384745f6fd69ab89a40f25
lebroninit <shaw.andrew.j@gmail.com>
2021-08-23 17:48:31 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("page loaded...");
+
+var namee = document.querySelector("#namee");
+
+function edit(){
+    namee.innerText = "Andrew Shaw";
+}
+
+var requ = document.querySelector("#requ");
+
+var funnn = document.querySelector("#funnn");
+
+var badg = document.querySelector("#badg");
+
+function click1(){
+    funnn.remove();
+    var temp = requ.innerText;
+    var temp1 = parseInt(temp);
+    temp1--;
+    requ.innerText = temp1 + "";
+
+    var temp2 = badg.innerText;
+    var temp3 = parseInt(temp2);
+    temp3++;
+    badg.innerText = temp3 + "";
+}
+
+function click12(){
+    funnn.remove();
+    var temp = requ.innerText;
+    var temp1 = parseInt(temp);
+    temp1--;
+    requ.innerText = temp1 + "";
+}
+
+var funn = document.querySelector("#funn");
+
+function clickk(){
+    funn.remove();
+    var temp = requ.innerText;
+    var temp1 = parseInt(temp);
+    temp1--;
+    requ.innerText = temp1 + "";
+
+    var temp2 = badg.innerText;
+    var temp3 = parseInt(temp2);
+    temp3++;
+    badg.innerText = temp3 + "";
+}
+
+function clickk1(){
+    funn.remove();
+    var temp = requ.innerText;
+    var temp1 = parseInt(temp);
+    temp1--;
+    requ.innerText = temp1 + "";
+}
